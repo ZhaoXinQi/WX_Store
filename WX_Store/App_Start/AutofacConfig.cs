c037bd556224a9ba4a0f6c325c6e28ba58c4db1e
Xinqi Zhao <974700216@qq.com>
2017-10-18 09:12:17 +0800

    添加autofacConfig类,并在全局里面把它设置为全局的autofacConfig

---
+++ WX_Store/App_Start/AutofacConfig.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;//需要饮用自动工厂类
+using Autofac.Integration.Mvc;//引用此类,注册工厂用
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Reflection;//需要饮用程序集类 assmbly使用
+using System.Web.Mvc;
+
+namespace WX_Store.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void autoDepence()
+        {
+            //先实例化一个构造器
+            var builder = new ContainerBuilder();
+            //注册使用方    后面需要使用 PropertiesAutowired()方法,把注册的编程使用方
+            builder.RegisterControllers(Assembly.GetExecutingAssembly()).PropertiesAutowired();
+            //通过程序集下面给定程序集的长格式名称加载程序集
+            var iRepository = Assembly.Load("Fish_IRepository");
+            var iService = Assembly.Load("IBaseService");
+            var Repository = Assembly.Load("Fish_Repository");
+            var Service = Assembly.Load("Fish_Service");
+            //注册实现方    where后面是匹配后面的结尾是否为指定的字符串结尾
+            builder.RegisterAssemblyTypes(iRepository, Repository).Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces();
+            builder.RegisterAssemblyTypes(iService, Service).Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces();
+            //创建容器
+            var container = builder.Build();
+            //依赖注入
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
--- WX_Store/Global.asax.cs
+++ WX_Store/Global.asax.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-
+using WX_Store.App_Start;
 namespace WX_Store
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            AutofacConfig.autoDepence();//把autoconfig定义成全局的变量
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
