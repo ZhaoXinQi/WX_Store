7f355625cf3492141bfbcf3d968be322171da571
Xinqi Zhao <974700216@qq.com>
2017-10-18 14:51:58 +0800

    Revert "编辑Indexbanner,但是未显示,显示为将对象引用实例"
    This reverts commit d94333c7e9f669212f30dd2e8c6ba0994de721de.

--- WX_Store/Controllers/HomeController.cs
+++ WX_Store/Controllers/HomeController.cs
@@ -3,22 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WxShop_Model;
-using Fish_IRepository;
-using IBaseService;
+
 namespace WX_Store.Controllers
 {
     public class HomeController : Controller
     {
-        /// <summary>
-        /// 以属性方式注入
-        /// </summary>
-        public IBannerService bannerService { get; set; }
         // GET: Home
         public ActionResult Index()
         {
-            var bannerResult = bannerService.GetEntities(x => true);
-            ViewBag.banner = bannerResult.ToList();
             return View();
         }
         public ActionResult Seach()
