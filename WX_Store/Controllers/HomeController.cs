cb24c43eb2bf9cf1bdea8a7beb8474594dbd0965
Xinqi Zhao <974700216@qq.com>
2017-10-18 16:18:43 +0800

    banner传值,banner传入前台,现在有错误:为将对象实例化

--- WX_Store/Controllers/HomeController.cs
+++ WX_Store/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WxShop_Model;
+using Fish_IRepository;
+using IBaseService;
 namespace WX_Store.Controllers
 {
     public class HomeController : Controller
     {
+        private IBannerService bannerService { get; set; }
         // GET: Home
         public ActionResult Index()
         {
+            var GetBanner = bannerService.GetEntities(x => true);
+            ViewBag.Banner = GetBanner.ToList();
             return View();
         }
         public ActionResult Seach()
