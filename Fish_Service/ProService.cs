b36502a2d28f498ca8205a39824898a8d362466d
Xinqi Zhao <974700216@qq.com>
2017-10-19 15:52:41 +0800

    热销显示框架已搭好

---
+++ Fish_Service/ProService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WxShop_Model;
+using IBaseService;
+using Fish_IRepository;
+namespace Fish_Service
+{
+    public class ProService:BaseService<ProductInfo>,IProService
+    {
+        public ProService(IBaseRepository<ProductInfo> baseRepository):base(baseRepository)
+        {
+
+        }
+    }
+}
