98b1ce509345b787fc34ccd8a901dff25adcbd01
Xinqi Zhao <974700216@qq.com>
2017-10-23 15:14:07 +0800

    分类页添加

---
+++ WX_Store/Controllers/SortController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WX_Store.Controllers
+{
+    public class SortController : Controller
+    {
+        // GET: Sort
+        public ActionResult sort()
+        {
+            return View();
+        }
+    }
+}
