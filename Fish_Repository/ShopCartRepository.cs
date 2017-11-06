224389c2aa3a5012a4310e8321c424fdc0db23cf
Xinqi Zhao <974700216@qq.com>
2017-11-06 15:37:28 +0800

    购物车框架

---
+++ Fish_Repository/ShopCartRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WxShop_Model;
+using Fish_IRepository;
+namespace Fish_Repository
+{
+    public  class ShopCartRepository:BaseRepository<ShoppongCart>,IShopCartRepository
+    {
+    }
+}
---
+++ Fish_Service/ShopCartService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WxShop_Model;
+using IBaseService;
+using Fish_IRepository;
+namespace Fish_Service
+{
+    public class ShopCartService:BaseService<ShoppongCart>,IShopCartService
+    {
+        public ShopCartService(IBaseRepository<ShoppongCart> baseRepository) : base(baseRepository)
+        {
+
+        }
+    }
+}
